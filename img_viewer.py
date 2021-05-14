c3dc270a49db1df08246e9c5323bb3db4096c136
Durgesh Reddiyar <durgeshreddiyar@yahoo.com>
2021-05-14 23:19:31 +0530

    combined
    added extraction function to GUI. have to check now if that works

--- img_viewer.py
+++ img_viewer.py
@@ -2,6 +2,10 @@
 
 import PySimpleGUI as sg
 import os.path
+from PIL import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+import pandas as pd
 
 # First the window layout in 2 columns
 
@@ -39,6 +43,67 @@ layout = [
 
 window = sg.Window("Medical Form Data extractor", layout)
 
+
+
+def extract_data(filename):
+    img = filename
+    imge = Image.open(img)
+    imgGray = imge.convert('L')
+
+    mystring = pytesseract.image_to_string(imgGray)
+
+    data = mystring
+    df = pd.DataFrame([x.split(';') for x in data.split('\n')])
+
+    with open("Technology_list.txt", "r") as Technology_list:
+        lines = Technology_list.readlines()
+    T_list = [x.replace('\n', '') for x in lines]
+    #print(T_list)
+
+    with open("test_name_list.txt", "r") as test_name_list:
+        lines = test_name_list.readlines()
+    TN_list = [x.replace('\n', '') for x in lines]
+    #print(TN_list)
+
+    keywords = T_list + TN_list
+
+    # print(keywords)
+
+    raw_result = []
+    for index, row in df.iterrows():
+        #print(index,row[0])
+
+        text_string = row[0]
+        #print(text_string)
+        for keyword in keywords:
+            if keyword in text_string:
+                # print(index)
+                # print(text_string)
+                raw_result.append(text_string)
+                if keyword in TN_list:
+                    print("Test Name Match:",keyword)  
+                elif keyword in T_list:
+                    print("Technology match:",keyword)
+                
+
+    #print(raw_result)
+
+    with open("methods.txt", "r") as methods_list:
+        lines = methods_list.readlines()
+    method_list = [x.replace('\n', '') for x in lines]
+
+    for method in method_list:
+        while method in raw_result: raw_result.remove(method)  
+
+    #print(raw_result)
+
+    raw_result_df = pd.DataFrame(raw_result)
+    print(raw_result_df)
+
+
+
+
+
 # Run the Event Loop
 while True:
     event, values = window.read()
@@ -67,6 +132,9 @@ while True:
             )
             window["-TOUT-"].update(filename)
             window["-IMAGE-"].update(filename=filename)
+
+            extract_data(filename)
+           
 
         except:
             pass
